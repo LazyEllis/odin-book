7a2e55e1cbfc0e5f827618d7c36cef9550001d2a
Emmanuel Danison <117293986+LazyEllis@users.noreply.github.com>
2026-08-23 00:27:14 +0100

    Enable user interactions on timeline posts

--- src/components/ui/TimelinePost.tsx
+++ src/components/ui/TimelinePost.tsx
@@ -8,7 +8,7 @@ import {
   BookmarkIcon,
 } from "@heroicons/react/24/outline";
 import type { PostPublic } from "../../interfaces/api";
-import { formatTimestamp } from "../../utils/format";
+import { classNames, formatTimestamp } from "../../utils/format";
 import {
   bookmarkPost,
   likePost,
@@ -88,7 +88,7 @@ const TimelinePost: FC<IComponentProps> = ({ post, queryKey }) => {
                 ...post,
                 _count: {
                   ...post._count,
-                  likes: post.interactionStatus.isBookmarked
+                  bookmarks: post.interactionStatus.isBookmarked
                     ? post._count.bookmarks - 1
                     : post._count.bookmarks + 1,
                 },
@@ -103,7 +103,7 @@ const TimelinePost: FC<IComponentProps> = ({ post, queryKey }) => {
     },
   });
 
-  const handleRepostToggle = () => likeMutation.mutate(post.id);
+  const handleRepostToggle = () => repostMutation.mutate(post.id);
 
   const handleLikeToggle = () => likeMutation.mutate(post.id);
 
@@ -111,7 +111,7 @@ const TimelinePost: FC<IComponentProps> = ({ post, queryKey }) => {
 
   return (
     <div className="border-b border-white/10">
-      <article className="flex cursor-pointer overflow-hidden px-4 hover:bg-white/3">
+      <article className="flex cursor-pointer overflow-hidden px-4 py-3 hover:bg-white/3">
         <div className="mr-2 grow-0 basis-10 items-center">
           <Link
             aria-hidden
@@ -126,7 +126,7 @@ const TimelinePost: FC<IComponentProps> = ({ post, queryKey }) => {
             />
           </Link>
         </div>
-        <div className="grow basis-0 justify-center pb-3">
+        <div className="grow basis-0 justify-center">
           <div className="mb-0.5">
             <div className="flex items-start justify-between">
               <div>
@@ -146,6 +146,96 @@ const TimelinePost: FC<IComponentProps> = ({ post, queryKey }) => {
             </div>
           </div>
           <div>{post.text}</div>
+          <div className="mt-3 flex justify-between gap-x-1">
+            <button
+              aria-label={`${post._count.replies} ${post._count.replies === 1 ? "reply" : "replies"}. Reply`}
+              className="group hover:text-reply flex min-h-5 cursor-pointer justify-center overflow-visible text-gray-400"
+            >
+              <div className="flex min-w-0 items-center justify-start wrap-break-word whitespace-nowrap">
+                <div className="relative inline-flex">
+                  <div className="group-hover:bg-reply/10 absolute inset-0 -m-2 inline-flex rounded-full"></div>
+                  <ChatBubbleOvalLeftIcon className="size-4.75 text-current" />
+                </div>
+                {post._count.replies > 0 && (
+                  <div className="inline-flex overflow-hidden pl-1 text-sm">
+                    {post._count.replies}
+                  </div>
+                )}
+              </div>
+            </button>
+
+            <button
+              onClick={handleRepostToggle}
+              disabled={repostMutation.isPending}
+              aria-label={`${post._count.reposts} ${post._count.reposts === 1 ? "repost" : "reposts"}. Repost`}
+              className={classNames(
+                "group hover:text-repost flex min-h-5 cursor-pointer justify-center overflow-visible",
+                post.interactionStatus.isReposted
+                  ? "text-repost"
+                  : "text-gray-400",
+              )}
+            >
+              <div className="flex min-w-0 items-center justify-start wrap-break-word whitespace-nowrap">
+                <div className="relative inline-flex">
+                  <div className="group-hover:bg-repost/10 absolute inset-0 -m-2 inline-flex rounded-full"></div>
+                  <ArrowPathRoundedSquareIcon className="size-4.75" />
+                </div>
+                {post._count.reposts > 0 && (
+                  <div className="inline-flex overflow-hidden pl-1 text-sm">
+                    {post._count.reposts}
+                  </div>
+                )}
+              </div>
+            </button>
+
+            <button
+              onClick={handleLikeToggle}
+              disabled={likeMutation.isPending}
+              aria-label={`${post._count.likes} ${post._count.likes === 1 ? "like" : "likes"}. Like`}
+              className={classNames(
+                "group hover:text-like flex min-h-5 cursor-pointer justify-center overflow-visible",
+                post.interactionStatus.isLiked
+                  ? "text-like fill-like"
+                  : "text-gray-400",
+              )}
+            >
+              <div className="flex min-w-0 items-center justify-start wrap-break-word whitespace-nowrap">
+                <div className="relative inline-flex">
+                  <div className="group-hover:bg-like/10 absolute inset-0 -m-2 inline-flex rounded-full"></div>
+                  <HeartIcon className="size-4.75 fill-inherit" />
+                </div>
+                {post._count.likes > 0 && (
+                  <div className="inline-flex overflow-hidden pl-1 text-sm">
+                    {post._count.likes}
+                  </div>
+                )}
+              </div>
+            </button>
+
+            <button
+              onClick={handleBookmarkToggle}
+              disabled={bookmarkMutation.isPending}
+              aria-label={`${post._count.bookmarks} ${post._count.bookmarks === 1 ? "bookmark" : "bookmarks"}. Bookmark`}
+              className={classNames(
+                "group hover:text-bookmark flex min-h-5 cursor-pointer justify-center overflow-visible",
+                post.interactionStatus.isBookmarked
+                  ? "text-bookmark fill-bookmark"
+                  : "text-gray-400",
+              )}
+            >
+              <div className="flex min-w-0 items-center justify-start wrap-break-word whitespace-nowrap">
+                <div className="relative inline-flex">
+                  <div className="group-hover:bg-bookmark/10 absolute inset-0 -m-2 inline-flex rounded-full"></div>
+                  <BookmarkIcon className="size-4.75 fill-inherit" />
+                </div>
+                {post._count.bookmarks > 0 && (
+                  <div className="inline-flex overflow-hidden pl-1 text-sm">
+                    {post._count.bookmarks}
+                  </div>
+                )}
+              </div>
+            </button>
+          </div>
         </div>
       </article>
     </div>
